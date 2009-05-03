6e3ec5c8edd043834705c79c76130ec9ddef4f34
TJ Vanderpoel <bougy.man@gmail.com>
2009-05-03 12:01:11 -0500

    Version 3.1.8 fixes nag warning about resetting Autumn::ROOT

--- lib/autumn.rb
+++ lib/autumn.rb
@@ -1,6 +1,8 @@
 require "pathname"
 module Autumn
-  ROOT = Pathname.new(__FILE__).dirname.expand_path
+  unless Autumn.const_defined?("ROOT")
+    ROOT = Pathname.new(__FILE__).dirname.expand_path
+  end
 end
 $LOAD_PATH.unshift Autumn::ROOT
 require "autumn/version"
